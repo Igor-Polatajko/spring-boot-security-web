b62ea51ee016b2ad23fabe1b81bebb933c0284f6
Ihor Polataiko <igor.bogdanovich39@gmail.com>
2024-12-09 02:12:33 +0200

    Stateless session creation policy

--- src/main/java/com/ihorpolataiko/springbootsecurityweb/config/SecurityConfig.java
+++ src/main/java/com/ihorpolataiko/springbootsecurityweb/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -61,7 +62,8 @@ public class SecurityConfig {
                     .permitAll())
         .authorizeHttpRequests(matcher -> matcher.anyRequest().authenticated())
         .csrf(AbstractHttpConfigurer::disable)
-        .sessionManagement(AbstractHttpConfigurer::disable)
+        .sessionManagement(
+            configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .exceptionHandling(
             customizer ->
                 customizer
